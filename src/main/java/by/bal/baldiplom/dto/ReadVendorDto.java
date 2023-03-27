1087d23cd6909b658f89ae4780813c3c3349ef73
Pavel Bal <balpoi000@gmail.com>
2023-03-27 17:23:24 +0300

    feat: add vendor DTOs

---
+++ src/main/java/by/bal/baldiplom/dto/ReadVendorDto.java
@@ -0,0 +1,9 @@
+package by.bal.baldiplom.dto;
+
+import lombok.Data;
+
+@Data
+public class ReadVendorDto {
+    int id;
+    String name;
+}
---
+++ src/main/java/by/bal/baldiplom/dto/WriteVendorDto.java
@@ -0,0 +1,8 @@
+package by.bal.baldiplom.dto;
+
+import lombok.Data;
+
+@Data
+public class WriteVendorDto {
+    String name;
+}
